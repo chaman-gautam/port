27be5308d5da52906a2a7072dffa5236b5ac3afe
chaman-gautam <chaman12gautam12@gmai.com>
2025-09-09 21:39:37 +0530

    update access link of php form

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -21,7 +21,7 @@ export default function Contactform() {
 
     try {
       const res = await axios.post(
-        "http://localhost/backend/contact.php",
+        "https://port.infinityfreeapp.com/",
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
